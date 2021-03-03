fac22bb09a6b3a2aa670172d97528fc13c7af0f6
Ata Can Türk <atacanturk97@gmail.com>
2021-03-03 16:54:08 +0300

    bug fixed

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -16,8 +16,20 @@ namespace Business.Concrete
             _productDal = productDal;
         }
         public List<Product> GetAll()
-        {
+        {   
+            //buraya iş kodları yazılır(if böyleyse, if şöyleyse vs.) 
+            //yetkisi var mı?
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -52,8 +53,18 @@ namespace DataAccess.Concrete.InMemory
         }
         public List<Product> GetAllByCategory(int categoryId)
         {
-            throw new NotImplementedException();
+            
             return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
